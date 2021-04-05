97db2e0e1807ec1698790e69301a35b40eb8d2d6
M-Nardi <marcellonardi09@gmail.com>
2021-04-05 17:03:11 -0300

    First-Model-Controller-View - Department & Documentações

--- SalesWebMvc/SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/SalesWebMvc/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC Project";
+            ViewData["Professor"] = "Nélio Alves";
 
             return View();
         }
