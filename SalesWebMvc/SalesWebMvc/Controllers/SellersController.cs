8e564c72d4b9fcdaa08a94de74a267241facfdd3
M-Nardi <marcellonardi09@gmail.com>
2021-04-08 18:37:20 -0300

    Criação de formulário - inserção de novos vendedores

--- SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMvc.Services;
+using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Controllers
 {
@@ -23,5 +24,19 @@ namespace SalesWebMvc.Controllers
             var list = _sellerService.FindAll();
             return View(list);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost] //declarando q o método é post
+        [ValidateAntiForgeryToken] //previne ataques CSRF - aproveitamento de seção para enviar dados maliciosos
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller); //redirecionar a ação para o metodo index do SellersController (mesma classe)
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
